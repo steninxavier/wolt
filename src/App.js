41b262fc82315dd487df1627ebb7517693ad87e2
steninxavier <67514601+steninxavier@users.noreply.github.com>
2021-01-26 14:43:31 +0100

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import profilepicture from './img/pp.jfif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stenin xavier</h1>
+      <h5>A passionate web developer</h5>
+      <img src={profilepicture} alt="stenin"/>
+    </div>
+  );
+}
+
+export default App;
